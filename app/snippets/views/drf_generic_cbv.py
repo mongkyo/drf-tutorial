d7271f7281c4646c52df4b0b8617376f8d4d63c3
JungMongKyo <dreamong91@gmail.com>
2018-11-13 16:26:04 +0900

    SnippetCreate시 인증 permission추가

--- app/snippets/views/drf_generic_cbv.py
+++ app/snippets/views/drf_generic_cbv.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from ..models import Snippet
 from ..serializers import SnippetSerializer
@@ -7,6 +7,13 @@ from ..serializers import SnippetSerializer
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+    )
+
+    def perform_create(self, serializer):
+        # 저장시점에 owner항목을 명시적으로 지정
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
